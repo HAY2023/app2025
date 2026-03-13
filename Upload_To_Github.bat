5f8e0916d50cab7ce2548a1661368b32465c613c
HAY2023 <214768469+HAY2023@users.noreply.github.com>
2026-03-13 15:23:24 +0100

    Fix CI: install Android SDK on runner

--- Upload_To_Github.bat
+++ Upload_To_Github.bat
@@ -5,8 +5,8 @@ echo         جاري تجهيز المشروع ورفعه إلى GitHub
 echo         لإنشاء نسخة APK في قسم الإصدارات (Releases)
 echo ----------------------------------------------------
 git add .
-git commit -m "Fix CI: update Gradle setup and actions"
-git tag -f v1.0.3
+git commit -m "Fix CI: Android SDK setup"
+git tag -f v1.0.4
 git push origin main --tags --force
 echo ----------------------------------------------------
 echo تم الرفع بنجاح! 
