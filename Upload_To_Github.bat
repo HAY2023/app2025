ff283f26e8f391a2cee4b9b111352f36d63fa47e
HAY2023 <214768469+HAY2023@users.noreply.github.com>
2026-03-14 14:13:05 +0100

    Patch v1.0.9

--- Upload_To_Github.bat
+++ Upload_To_Github.bat
@@ -1,7 +1,7 @@
 @echo off
 echo ====================================================
 echo    Uploading MasjidTV App to GitHub...
-echo    رفع التطبيق إلى GitHub (الإصدار 1.0.8)
+echo    رفع التطبيق إلى GitHub (الإصدار 1.0.9)
 echo ====================================================
 cd /d h:\app2025
 
@@ -9,10 +9,10 @@ echo [1] Adding files...
 git add -A
 
 echo [2] Committing changes...
-git commit -m "Patch v1.0.8"
+git commit -m "Patch v1.0.9"
 
-echo [3] Tagging release v1.0.8...
-git tag -f v1.0.8
+echo [3] Tagging release v1.0.9...
+git tag -f v1.0.9
 
 echo [4] Pushing to GitHub...
 git push origin main --tags --force
